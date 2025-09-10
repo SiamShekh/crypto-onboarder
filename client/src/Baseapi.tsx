e846852370b4247a821d034557a736add1ff37fe
Siam Sheikh <siam62349@gmail.com>
2025-09-10 12:48:29 +0600

    api changed

--- client/src/Baseapi.tsx
+++ client/src/Baseapi.tsx
@@ -4,8 +4,8 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 const BaseApi = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        // baseUrl: "https://api.whybuy.fun",
-        baseUrl: "http://localhost:3000",
+        baseUrl: "https://api.whybuy.fun",
+        // baseUrl: "http://localhost:3000",
         credentials: "include"
     }),
     endpoints: () => ({}),
